a1a50383397ff4a50f100b4baa75f6e4a0775bc5
Flaberson Barros <flainno@gmail.com>
2021-03-25 20:19:19 -0300

    Ajustes controlador

--- src/main/java/br/com/flaberson/temperatura/HttpConnection.java
+++ src/main/java/br/com/flaberson/temperatura/HttpConnection.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.json.JSONObject;
 
@@ -14,11 +16,13 @@ public class HttpConnection {
 	
 	public JSONObject requestServico(String Url) throws IOException { 
 		URL url = new URL(Url);
+		Logger logger = Logger.getLogger(HttpConnection.class.getName());
+		
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		
 		if (conn.getResponseCode() != 200) {
-			System.out.print("HTTP error code : " + conn.getResponseCode());
+			logger.log(Level.WARNING, "HTTP error code : " + conn.getResponseCode());
 		}
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
--- src/main/java/br/com/flaberson/temperatura/TemperaturaController.java
+++ src/main/java/br/com/flaberson/temperatura/TemperaturaController.java
@@ -1,39 +1,42 @@
 package br.com.flaberson.temperatura;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/wather",produces = "application/json")
 public class TemperaturaController {
 	
 	private String token = "88989b3b57b40f9f7f90f588aafb4006";
 	private int temperatura;
 	private String cidade; 
+	final String URL_API = "http://apiadvisor.climatempo.com.br/api/v1";
 
     @GetMapping
-    public ResponseEntity<String> get(@RequestParam("longitude") String longitude,@RequestParam("latitude") String latitude) throws IOException{
+    public ResponseEntity<String> get(@RequestParam(value="longitude", required=false) String longitude,@RequestParam(value = "latitude", required=false) String latitude) throws IOException{
+    	
+    	if(null==longitude || null==latitude) {
+    		JSONObject json = new JSONObject();
+        	json.put("status", "erro");
+        	json.put("msg", "Informe a latitude e longitude");    		
+            return new ResponseEntity<>(json.toString(), HttpStatus.OK);
+    	}     	
     	
     	HttpConnection http = new HttpConnection();
     	
     	//CONSULTA CIDADE PELA LONGITUDE E LATITUDE
-    	JSONObject jsonCidade = http.requestServico("http://apiadvisor.climatempo.com.br/api/v1/locale/city?latitude="+latitude+"&longitude="+longitude+"&token="+this.token);
+    	JSONObject jsonCidade = http.requestServico(URL_API + "/locale/city?latitude="+latitude+"&longitude="+longitude+"&token="+this.token);
     	int idCidade = jsonCidade.getInt("id");
     	
     	//CONSULTA CIDADE PELA LONGITUDE E LATITUDE
-    	JSONObject jsonTemperatura = http.requestServico("http://apiadvisor.climatempo.com.br/api/v1/weather/locale/"+idCidade+"/current?token="+this.token);
-    	
+    	JSONObject jsonTemperatura = http.requestServico(URL_API + "/weather/locale/"+idCidade+"/current?token="+this.token);
 		
 		JSONObject data = jsonTemperatura.getJSONObject("data");
 		temperatura = data.getInt("temperature");
@@ -45,6 +48,7 @@ public class TemperaturaController {
     	json.put("cidade", cidade);
 		
         return new ResponseEntity<>(json.toString(), HttpStatus.OK);
+    	
     }
 
 }
